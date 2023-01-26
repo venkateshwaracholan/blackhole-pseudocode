1be25b637cf2360eca11a0dc3c3ed3d8602834f5
venkateshwaran shanmugham <venkateshwaracholan@gmail.com>
2023-01-27 05:24:55 +0530

    getting ready

---
+++ blackhole-pseudocode/src/read_before_interview/ReadBeforeInterview.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package read_before_interview;
+
+/**
+ *
+ * @author venka
+ */
+
+/*
+
+for DS and algo:
+
+ask constraints for problems
+method input and outputs
+reiterate problem and confirm
+expect followups and addd extensible code, write object oriented, create custom classes,
+write time complexity and space complexity
+proper variable names
+breakdown as functions
+concurrency for problems
+
+
+concurrency + concurrent resources
+
+for system design:
+
+draw beautiful for system design
+ask and write constraints, functional(usecase) and non functional requirements(scalability and config)
+scale, active users, compute traffic and data size(like char count for tweet and image max size) back of the envelope calc
+usecase + single machine micro service
+APIs 
+scale - ap -> LB + multiple nodes  
+db => master slave, warmed up vs cold, read replica,geo sharding(based on region or user id), cache for read heavy, write heavy take async.
+db indexing
+if read write heavy => its a trradeoff
+tradeoff and decision
+cap theorem databases, ca, cp ,ap
+monitoring and alerting
+
+
+
+time series database
+bloom filter
+probablistic data structures
+
+top k problem, priorityqueue, mapreduce to scale, 
+chess game, board game,  findpossibilities, move, 
+
+*/
+
+public class ReadBeforeInterview {
+    
+}
